93b4e47ada038037df4c2c43323d869bad68b434
triplelog <robmwilc@gmail.com>
2019-09-19 15:34:07 -0400

    f

--- js/boxscore.js
+++ js/boxscore.js
@@ -13,7 +13,7 @@ class BoxScore extends HTMLElement {
 
     jsonFile.onreadystatechange = function() {
         if (jsonFile.readyState== 4 && jsonFile.status == 200) {
-            _this.offbox(Papa.parse(jsonFile.responseText));
+            _this.offbox(Papa.parse(jsonFile.responseText).data);
         }
      }
 	let template = document.getElementById('boxscore');
