18f4007f921291820f50980b9269aa5d10b30efc
triplelog <robmwilc@gmail.com>
2019-09-20 08:55:47 -0400

    f

--- js/boxscore.js
+++ js/boxscore.js
@@ -26,14 +26,15 @@ class BoxScore extends HTMLElement {
             _this.pitchbox(Papa.parse(jsonFile2.responseText).data);
         }
      }
-    var url3 = 'box/ATL197309260plays.csv';
-	var jsonFile3 = new XMLHttpRequest();
-    jsonFile3.open("GET",url3,true);
-    jsonFile3.send();
+    
+    var url4 = 'box/ATL197309260.txt';
+	var jsonFile4 = new XMLHttpRequest();
+    jsonFile4.open("GET",url4,true);
+    jsonFile4.send();
 
-    jsonFile3.onreadystatechange = function() {
-        if (jsonFile3.readyState== 4 && jsonFile3.status == 200) {
-            _this.linescore(Papa.parse(jsonFile3.responseText).data);
+    jsonFile4.onreadystatechange = function() {
+        if (jsonFile4.readyState== 4 && jsonFile4.status == 200) {
+            _this.linescore(Papa.parse(jsonFile4.responseText).data);
         }
      }
      
@@ -63,108 +64,125 @@ class BoxScore extends HTMLElement {
 
   }
   
-  linescore(linescoreRaw) {
-  	var linescore = this.shadowRoot.querySelector('#linescore-location');
-  	var thead = linescore.querySelector('thead').querySelector('tr');
-  	var row1 = linescore.querySelector('tbody').querySelectorAll('tr')[0];
-  	var row2 = linescore.querySelector('tbody').querySelectorAll('tr')[1];
-  	thead.innerHTML = '';
-  	row1.innerHTML = '';
-  	row2.innerHTML = '';
-  	
+  linescore(gameinfo) {
   	var awayteam = "Away";
-  	var hometeam = "Home";
-  	var pbpLength = linescoreRaw.length;
-	  var linescorearray = [[awayteam],[hometeam]];
-	  console.log(linescorearray);
-	  var currentTeam = '1';
-	  var halfRuns = 0;
-	  var halfIndex = -1;
-	  var currentouts = 0;
-	  var totals = [[0,0,0],[0,0,0]]
-	  for (var i=1;i<pbpLength;i++) {
-		  if (linescoreRaw[i][15] == '0' && currentTeam == '1') {
-			halfRuns = 0;
-			halfIndex += 1;
-			linescorearray[0].push(0);
-			currentouts = 0;
-		  }
-		  else if (linescoreRaw[i][15] == '1' && currentTeam == '0') {
-			halfRuns = 0;
-			halfIndex += 1;
-			linescorearray[1].push(0);
-			currentouts = 0;
-		  }
-		  if (parseInt(linescoreRaw[i][12]) > 0) {
-			halfRuns += parseInt(linescoreRaw[i][12]);
-			linescorearray[halfIndex%2][parseInt(halfIndex/2)+1] = halfRuns;
-			totals[halfIndex%2][0] += parseInt(linescoreRaw[i][12]);
-		  }
-		  if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('singles') > -1) {
-		  	totals[halfIndex%2][1] += 1;
-		  }
-		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('doubles') > -1) {
-		  	totals[halfIndex%2][1] += 1;
-		  }
-		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('triples') > -1) {
-		  	totals[halfIndex%2][1] += 1;
-		  }
-		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('homers') > -1) {
-		  	totals[halfIndex%2][1] += 1;
-		  }
-		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('error') > -1) {
-		  	totals[1 - (halfIndex%2)][2] += 1;
-		  }
-		  
-	  	  if (parseInt(linescoreRaw[i][11]) > 0) {
-			currentouts += parseInt(linescoreRaw[i][11]);
-		  }
-		  currentTeam = linescoreRaw[i][15];
-	  }
-	  linescorearray[0].push(totals[0][0]);
-	  linescorearray[1].push(totals[1][0]);
-	  linescorearray[0].push(totals[0][1]);
-	  linescorearray[1].push(totals[1][1]);
-	  linescorearray[0].push(totals[0][2]);
-	  linescorearray[1].push(totals[1][2]);
-	  
-	  
-  	var th = document.createElement('th');
-  	th.textContent = 'Team';
-  	thead.appendChild(th);
-  	var td = document.createElement('td');
-  	td.textContent = linescorearray[0][0];
-  	row1.appendChild(td);
-  	td = document.createElement('td');
-  	td.textContent = linescorearray[1][0];
-  	row2.appendChild(td);
-  	for (var i=1;i<linescorearray[0].length-3;i++) {
-  		th = document.createElement('th');
-		th.textContent = i;
-		thead.appendChild(th);
-		td = document.createElement('td');
-		td.textContent = linescorearray[0][i];
-		td.id = 'pbpHalf_A-'+i;
-		td.addEventListener('click', e => {alert(e.target.id);});
-		row1.appendChild(td);
-		td = document.createElement('td');
-		td.textContent = linescorearray[1][i];
-		td.id = 'pbpHalf_H-'+i;
-		td.addEventListener('click', e => {alert(e.target.id);});
-		row2.appendChild(td);
+	var hometeam = "Home";
+  	for (var i=0;i<gameinfo.length;i++){
+  		if (gameinfo[i][1] == 'visteam') {awayteam = gameinfo[i][2];}
+  		else if (gameinfo[i][1] == 'hometeam') {hometeam = gameinfo[i][2];}
   	}
-  	['R','H','E'].forEach( x => {
-  		th = document.createElement('th');
-		th.textContent = x;
-		thead.appendChild(th);
-		td = document.createElement('td');
-		td.textContent = linescorearray[0][i];
-		row1.appendChild(td);
-		td = document.createElement('td');
-		td.textContent = linescorearray[1][i];
-		row2.appendChild(td);
-		i++;
-  	});
+  	
+  	var url = 'box/ATL197309260plays.csv';
+	var jsonFile = new XMLHttpRequest();
+    jsonFile.open("GET",url,true);
+    jsonFile.send();
+
+    jsonFile.onreadystatechange = function() {
+        if (jsonFile.readyState== 4 && jsonFile.status == 200) {
+            linescoreRaw = Papa.parse(jsonFile.responseText).data;
+            var linescore = this.shadowRoot.querySelector('#linescore-location');
+			var thead = linescore.querySelector('thead').querySelector('tr');
+			var row1 = linescore.querySelector('tbody').querySelectorAll('tr')[0];
+			var row2 = linescore.querySelector('tbody').querySelectorAll('tr')[1];
+			thead.innerHTML = '';
+			row1.innerHTML = '';
+			row2.innerHTML = '';
+	
+			
+			var pbpLength = linescoreRaw.length;
+			  var linescorearray = [[awayteam],[hometeam]];
+			  console.log(linescorearray);
+			  var currentTeam = '1';
+			  var halfRuns = 0;
+			  var halfIndex = -1;
+			  var currentouts = 0;
+			  var totals = [[0,0,0],[0,0,0]]
+			  for (var i=1;i<pbpLength;i++) {
+				  if (linescoreRaw[i][15] == '0' && currentTeam == '1') {
+					halfRuns = 0;
+					halfIndex += 1;
+					linescorearray[0].push(0);
+					currentouts = 0;
+				  }
+				  else if (linescoreRaw[i][15] == '1' && currentTeam == '0') {
+					halfRuns = 0;
+					halfIndex += 1;
+					linescorearray[1].push(0);
+					currentouts = 0;
+				  }
+				  if (parseInt(linescoreRaw[i][12]) > 0) {
+					halfRuns += parseInt(linescoreRaw[i][12]);
+					linescorearray[halfIndex%2][parseInt(halfIndex/2)+1] = halfRuns;
+					totals[halfIndex%2][0] += parseInt(linescoreRaw[i][12]);
+				  }
+				  if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('singles') > -1) {
+					totals[halfIndex%2][1] += 1;
+				  }
+				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('doubles') > -1) {
+					totals[halfIndex%2][1] += 1;
+				  }
+				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('triples') > -1) {
+					totals[halfIndex%2][1] += 1;
+				  }
+				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('homers') > -1) {
+					totals[halfIndex%2][1] += 1;
+				  }
+				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('error') > -1) {
+					totals[1 - (halfIndex%2)][2] += 1;
+				  }
+		  
+				  if (parseInt(linescoreRaw[i][11]) > 0) {
+					currentouts += parseInt(linescoreRaw[i][11]);
+				  }
+				  currentTeam = linescoreRaw[i][15];
+			  }
+			  linescorearray[0].push(totals[0][0]);
+			  linescorearray[1].push(totals[1][0]);
+			  linescorearray[0].push(totals[0][1]);
+			  linescorearray[1].push(totals[1][1]);
+			  linescorearray[0].push(totals[0][2]);
+			  linescorearray[1].push(totals[1][2]);
+	  
+	  
+			var th = document.createElement('th');
+			th.textContent = 'Team';
+			thead.appendChild(th);
+			var td = document.createElement('td');
+			td.textContent = linescorearray[0][0];
+			row1.appendChild(td);
+			td = document.createElement('td');
+			td.textContent = linescorearray[1][0];
+			row2.appendChild(td);
+			for (var i=1;i<linescorearray[0].length-3;i++) {
+				th = document.createElement('th');
+				th.textContent = i;
+				thead.appendChild(th);
+				td = document.createElement('td');
+				td.textContent = linescorearray[0][i];
+				td.id = 'pbpHalf_A-'+i;
+				td.addEventListener('click', e => {alert(e.target.id);});
+				row1.appendChild(td);
+				td = document.createElement('td');
+				td.textContent = linescorearray[1][i];
+				td.id = 'pbpHalf_H-'+i;
+				td.addEventListener('click', e => {alert(e.target.id);});
+				row2.appendChild(td);
+			}
+			['R','H','E'].forEach( x => {
+				th = document.createElement('th');
+				th.textContent = x;
+				thead.appendChild(th);
+				td = document.createElement('td');
+				td.textContent = linescorearray[0][i];
+				row1.appendChild(td);
+				td = document.createElement('td');
+				td.textContent = linescorearray[1][i];
+				row2.appendChild(td);
+				i++;
+			});
+        }
+     }
+  	
   	
   }
   offbox(batterarray) {
@@ -232,7 +250,7 @@ class BoxScore extends HTMLElement {
   	tbodya.innerHTML = '';
   	theadh.innerHTML = '';
   	tbodyh.innerHTML = '';
-  	['Name','IPouts','H','BB','R','ER','K'].forEach(x => {
+  	['Name','IP','H','BB','R','ER','K'].forEach(x => {
   		var th = document.createElement('th');
 		th.textContent = x;
 		theada.appendChild(th);
@@ -250,7 +268,16 @@ class BoxScore extends HTMLElement {
 
 		[0,2,3,4,5,6,8].forEach( x => {
 			var td = document.createElement('td');
-			td.textContent = pitcherarray[i][x];
+			if (x != 2){td.textContent = pitcherarray[i][x];}
+			else {
+				var IPouts = parseInt(pitcherarray[i][x]);
+				if (IPouts%3 == 0) {td.textContent = IPouts/3;}
+				else if (IPouts%3 == 1 && IPouts > 3) {td.textContent = parseInt((IPouts-1)/3) + ' ⅓';}
+				else if (IPouts%3 == 2 && IPouts > 3) {td.textContent = parseInt((IPouts-2)/3) + ' ⅔';}
+				else if (IPouts%3 == 1) {td.textContent = '⅓';}
+				else if (IPouts%3 == 2) {td.textContent = '⅔';}
+				
+			}
 			tr.appendChild(td);
 		});
 		tr.addEventListener('click', e => {alert(e.target.parentNode.id);});
