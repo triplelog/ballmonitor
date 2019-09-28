e02965ebaff618c13bbb3a8e9c8e283ef4ad7f0e
triplelog <robmwilc@gmail.com>
2019-09-27 22:38:23 -0400

    f

--- js/boxscore.js
+++ js/boxscore.js
@@ -11,8 +11,10 @@ class BoxScore extends HTMLElement {
     let templateContent = template.content;
     
     const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(templateContent.cloneNode(true));
+    this.dataFiles = {prev:{id:null},current:{id:null},next:{id:null}};
     this.chgsrc();
-    this.returnedFiles = 0;
+    
+    
 
 	
 
@@ -20,54 +22,112 @@ class BoxScore extends HTMLElement {
   
   chgsrc() {
   	var _this = this;
-    this.gameid = this.getAttribute('src');
-    if (this.gameid == null) {return 0;}
-    this.style.opacity = 0;
-    this.returnedFiles = 0;
-  	var url = 'box/2000/2000'+this.gameid.substring(0,3)+'/'+this.gameid+'batterbox.csv';
-	var jsonFile = new XMLHttpRequest();
-    jsonFile.open("GET",url,true);
-    jsonFile.send();
-
-    jsonFile.onloadend = function() {
-        if(jsonFile4.status == 404) {
-			_this.style.opacity = 0;
-		}
-		else if (jsonFile.status == 200) {
-            _this.offbox(Papa.parse(jsonFile.responseText).data);
-            _this.returnedFiles++;
-            if (_this.returnedFiles == 3) {_this.style.opacity = 1;}
-        }
-     }
-    var url2 = 'box/2000/2000'+this.gameid.substring(0,3)+'/'+this.gameid+'pitcherbox.csv';
-	var jsonFile2 = new XMLHttpRequest();
-    jsonFile2.open("GET",url2,true);
-    jsonFile2.send();
-	jsonFile2.onloadend = function() {
-		if(jsonFile2.status == 404) {
-			_this.style.opacity = 0;
-		}
-		else if (jsonFile2.status == 200) {
-            _this.pitchbox(Papa.parse(jsonFile2.responseText).data);
-            _this.returnedFiles++;
-            if (_this.returnedFiles == 3) {_this.style.opacity = 1;}
-        }
-	}
-
+  	if (this.getAttribute('src') == null) {return 0;}
+  	else {
+  		if (this.getAttribute('src') == this.dataFiles.prev.id) {
+  			if (this.getAttribute('nsrc') == this.dataFiles.current.id) {
+  				this.dataFiles.next = this.dataFiles.current;
+  			}
+  			else {
+  				this.dataFiles.next = {id:this.getAttribute('nsrc')};
+  			}
+  			this.dataFiles.current = this.dataFiles.prev;
+  			this.dataFiles.prev = {id:this.getAttribute('psrc')};
+  		}
+  		else if (this.getAttribute('src') == this.dataFiles.next.id) {
+  			if (this.getAttribute('psrc') == this.dataFiles.current.id) {
+  				this.dataFiles.prev = this.dataFiles.current;
+  			}
+  			else {
+  				this.dataFiles.prev = {id:this.getAttribute('psrc')};
+  			}
+  			this.dataFiles.current = this.dataFiles.next;
+  			this.dataFiles.next = {id:this.getAttribute('nsrc')};
+  		}
+  		else {
+  			this.dataFiles.prev = {id:this.getAttribute('psrc')};
+  			this.dataFiles.next = {id:this.getAttribute('nsrc')};
+  			this.dataFiles.current = {id:this.getAttribute('src')};
+  		}
+  	}
     
-    var url4 = 'box/2000/2000'+this.gameid.substring(0,3)+'/'+this.gameid+'.txt';
-	var jsonFile4 = new XMLHttpRequest();
-    jsonFile4.open("GET",url4,true);
-    jsonFile4.send();
+    this.style.opacity = 0;
+    if (!this.dataFiles.current.hasOwnProperty('batters')){
+    	var url = 'box/2000/2000'+this.dataFiles.current.id.substring(0,3)+'/'+this.dataFiles.current.id+'batterbox.csv';
+		var jsonFile = new XMLHttpRequest();
+		jsonFile.open("GET",url,true);
+		jsonFile.send();
 
-    jsonFile4.onloadend = function() {
-        if(jsonFile4.status == 404) {
-			_this.style.opacity = 0;
+		jsonFile.onloadend = function() {
+			if(jsonFile4.status == 404) {
+				_this.style.opacity = 0;
+			}
+			else if (jsonFile.status == 200) {
+				_this.dataFiles.current.batters = Papa.parse(jsonFile.responseText).data;
+				_this.offbox(_this.dataFiles.current.batters);
+				if (_this.dataFiles.current.hasOwnProperty('pitchers') && _this.dataFiles.current.hasOwnProperty('plays') && _this.dataFiles.current.hasOwnProperty('info')){
+					_this.style.opacity = 1;
+				}
+			}
+		 }
+    }
+    else {
+    	_this.offbox(_this.dataFiles.current.batters);
+		if (_this.dataFiles.current.hasOwnProperty('pitchers') && _this.dataFiles.current.hasOwnProperty('plays') && _this.dataFiles.current.hasOwnProperty('info')){
+			_this.style.opacity = 1;
 		}
-		else if (jsonFile4.status == 200) {
-            _this.linescore(Papa.parse(jsonFile4.responseText).data);
-        }
-     }
+    }
+    if (!this.dataFiles.current.hasOwnProperty('pitchers')){
+    	var url = 'box/2000/2000'+this.dataFiles.current.id.substring(0,3)+'/'+this.dataFiles.current.id+'pitcherbox.csv';
+		var jsonFile = new XMLHttpRequest();
+		jsonFile.open("GET",url,true);
+		jsonFile.send();
+
+		jsonFile.onloadend = function() {
+			if(jsonFile4.status == 404) {
+				_this.style.opacity = 0;
+			}
+			else if (jsonFile.status == 200) {
+				_this.dataFiles.current.pitchers = Papa.parse(jsonFile.responseText).data;
+				_this.pitchbox(_this.dataFiles.current.pitchers);
+				if (_this.dataFiles.current.hasOwnProperty('batters') && _this.dataFiles.current.hasOwnProperty('plays') && _this.dataFiles.current.hasOwnProperty('info')){
+					_this.style.opacity = 1;
+				}
+			}
+		 }
+    }
+    else {
+    	_this.pitchbox(_this.dataFiles.current.pitchers);
+		if (_this.dataFiles.current.hasOwnProperty('batters') && _this.dataFiles.current.hasOwnProperty('plays') && _this.dataFiles.current.hasOwnProperty('info')){
+			_this.style.opacity = 1;
+		}
+    }
+    if (!this.dataFiles.current.hasOwnProperty('info')){
+    	var url = 'box/2000/2000'+this.dataFiles.current.id.substring(0,3)+'/'+this.dataFiles.current.id+'.txt';
+		var jsonFile = new XMLHttpRequest();
+		jsonFile.open("GET",url,true);
+		jsonFile.send();
+
+		jsonFile.onloadend = function() {
+			if(jsonFile4.status == 404) {
+				_this.style.opacity = 0;
+			}
+			else if (jsonFile.status == 200) {
+				_this.dataFiles.current.info = Papa.parse(jsonFile.responseText).data;
+				_this.linescore(_this.dataFiles.current.info);
+				if (_this.dataFiles.current.hasOwnProperty('batters') && _this.dataFiles.current.hasOwnProperty('plays') && _this.dataFiles.current.hasOwnProperty('pitchers')){
+					_this.style.opacity = 1;
+				}
+			}
+		 }
+    }
+    else {
+    	_this.linescore(_this.dataFiles.current.info);
+		if (_this.dataFiles.current.hasOwnProperty('batters') && _this.dataFiles.current.hasOwnProperty('plays') && _this.dataFiles.current.hasOwnProperty('pitchers')){
+			_this.style.opacity = 1;
+		}
+    }
+
      
      
      
@@ -83,126 +143,139 @@ class BoxScore extends HTMLElement {
   		else if (gameinfo[i][1] == 'hometeam') {hometeam = gameinfo[i][2];}
   	}
   	
-  	var url = 'box/2000/2000'+this.gameid.substring(0,3)+'/'+this.gameid+'plays.csv';
-	var jsonFile = new XMLHttpRequest();
-    jsonFile.open("GET",url,true);
-    jsonFile.send();
+  	if (!this.dataFiles.current.hasOwnProperty('plays')){
+		var url = 'box/2000/2000'+this.gameid.substring(0,3)+'/'+this.gameid+'plays.csv';
+		var jsonFile = new XMLHttpRequest();
+		jsonFile.open("GET",url,true);
+		jsonFile.send();
 	
-    jsonFile.onreadystatechange = function() {
-        if (jsonFile.readyState== 4 && jsonFile.status == 200) {
-            var linescoreRaw = Papa.parse(jsonFile.responseText).data;
-            var linescore = _this.shadowRoot.querySelector('#linescore-location');
-			var thead = linescore.querySelector('thead').querySelector('tr');
-			var row1 = linescore.querySelector('tbody').querySelectorAll('tr')[0];
-			var row2 = linescore.querySelector('tbody').querySelectorAll('tr')[1];
-			thead.innerHTML = '';
-			row1.innerHTML = '';
-			row2.innerHTML = '';
-	
-			
-			var pbpLength = linescoreRaw.length;
-			  var linescorearray = [[awayteam],[hometeam]];
-			  var currentTeam = '1';
-			  var halfRuns = 0;
-			  var halfIndex = -1;
-			  var currentouts = 0;
-			  var totals = [[0,0,0],[0,0,0]]
-			  _this.shadowRoot.querySelector('#away-extra').innerHTML = '';
-			  _this.shadowRoot.querySelector('#home-extra').innerHTML = '';
-			  for (var i=1;i<pbpLength;i++) {
-				  if (linescoreRaw[i][15] == '0' && currentTeam == '1') {
-					halfRuns = 0;
-					halfIndex += 1;
-					linescorearray[0].push(0);
-					currentouts = 0;
-				  }
-				  else if (linescoreRaw[i][15] == '1' && currentTeam == '0') {
-					halfRuns = 0;
-					halfIndex += 1;
-					linescorearray[1].push(0);
-					currentouts = 0;
-				  }
-				  if (parseInt(linescoreRaw[i][12]) > 0) {
-					halfRuns += parseInt(linescoreRaw[i][12]);
-					linescorearray[halfIndex%2][parseInt(halfIndex/2)+1] = halfRuns;
-					totals[halfIndex%2][0] += parseInt(linescoreRaw[i][12]);
-				  }
-				  if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('singles') > -1) {
-					totals[halfIndex%2][1] += 1;
-				  }
-				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('doubles') > -1) {
-					totals[halfIndex%2][1] += 1;
-				  }
-				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('triples') > -1) {
-					totals[halfIndex%2][1] += 1;
-				  }
-				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('homers') > -1) {
-					totals[halfIndex%2][1] += 1;
-					if (halfIndex%2 == 0){_this.shadowRoot.querySelector('#away-extra').innerHTML += 'HR: '+linescoreRaw[i][14]+' ('+(linescoreRaw[i][12]-1)+' on)<br>';}
-					else if (halfIndex%2 == 1){_this.shadowRoot.querySelector('#home-extra').innerHTML += 'HR: '+linescoreRaw[i][14]+' ('+(linescoreRaw[i][12]-1)+' on)<br>';}
-				  }
-				  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('error') > -1) {
-					totals[1 - (halfIndex%2)][2] += 1;
-				  }
-		  
-				  if (parseInt(linescoreRaw[i][11]) > 0) {
-					currentouts += parseInt(linescoreRaw[i][11]);
-				  }
-				  currentTeam = linescoreRaw[i][15];
-			  }
-			  if (linescorearray[1].length < linescorearray[0].length) {linescorearray[1].push('x');}
-			  linescorearray[0].push(totals[0][0]);
-			  linescorearray[1].push(totals[1][0]);
-			  linescorearray[0].push(totals[0][1]);
-			  linescorearray[1].push(totals[1][1]);
-			  linescorearray[0].push(totals[0][2]);
-			  linescorearray[1].push(totals[1][2]);
-	  
-	  
-			var th = document.createElement('th');
-			th.textContent = '';
-			thead.appendChild(th);
-			var td = document.createElement('td');
-			td.textContent = linescorearray[0][0];
-			row1.appendChild(td);
-			td = document.createElement('td');
-			td.textContent = linescorearray[1][0];
-			row2.appendChild(td);
-			for (var i=1;i<linescorearray[0].length-3;i++) {
-				th = document.createElement('th');
-				th.textContent = i;
-				thead.appendChild(th);
-				td = document.createElement('td');
-				td.textContent = linescorearray[0][i];
-				td.id = 'pbpHalf_A-'+i;
-				td.addEventListener('click', e => {alert(e.target.id);});
-				row1.appendChild(td);
-				td = document.createElement('td');
-				td.textContent = linescorearray[1][i];
-				td.id = 'pbpHalf_H-'+i;
-				td.addEventListener('click', e => {alert(e.target.id);});
-				row2.appendChild(td);
+		jsonFile.onreadystatechange = function() {
+			if (jsonFile.readyState== 4 && jsonFile.status == 200) {
+				_this.dataFiles.current.plays = Papa.parse(jsonFile.responseText).data;
+				_this.fillscore(_this.dataFiles.current.plays);
+				if (_this.dataFiles.current.hasOwnProperty('batters') && _this.dataFiles.current.hasOwnProperty('info') && _this.dataFiles.current.hasOwnProperty('pitchers')){
+					_this.style.opacity = 1;
+				}
 			}
-			['R','H','E'].forEach( x => {
-				th = document.createElement('th');
-				th.textContent = x;
-				thead.appendChild(th);
-				td = document.createElement('td');
-				td.textContent = linescorearray[0][i];
-				row1.appendChild(td);
-				td = document.createElement('td');
-				td.textContent = linescorearray[1][i];
-				row2.appendChild(td);
-				i++;
-			});
-			
-			_this.returnedFiles++;
-            if (_this.returnedFiles == 3) {_this.style.opacity = 1;}
-        }
-     }
+		 }
+	}
+	else {
+		_this.fillscore(_this.dataFiles.current.plays);
+		if (_this.dataFiles.current.hasOwnProperty('batters') && _this.dataFiles.current.hasOwnProperty('info') && _this.dataFiles.current.hasOwnProperty('pitchers')){
+			_this.style.opacity = 1;
+		}
+	}
   	
   	
   }
+  
+  fillscore(linescoreRaw) {
+	var linescore = _this.shadowRoot.querySelector('#linescore-location');
+	var thead = linescore.querySelector('thead').querySelector('tr');
+	var row1 = linescore.querySelector('tbody').querySelectorAll('tr')[0];
+	var row2 = linescore.querySelector('tbody').querySelectorAll('tr')[1];
+	thead.innerHTML = '';
+	row1.innerHTML = '';
+	row2.innerHTML = '';
+
+
+	var pbpLength = linescoreRaw.length;
+	  var linescorearray = [[awayteam],[hometeam]];
+	  var currentTeam = '1';
+	  var halfRuns = 0;
+	  var halfIndex = -1;
+	  var currentouts = 0;
+	  var totals = [[0,0,0],[0,0,0]]
+	  _this.shadowRoot.querySelector('#away-extra').innerHTML = '';
+	  _this.shadowRoot.querySelector('#home-extra').innerHTML = '';
+	  for (var i=1;i<pbpLength;i++) {
+		  if (linescoreRaw[i][15] == '0' && currentTeam == '1') {
+			halfRuns = 0;
+			halfIndex += 1;
+			linescorearray[0].push(0);
+			currentouts = 0;
+		  }
+		  else if (linescoreRaw[i][15] == '1' && currentTeam == '0') {
+			halfRuns = 0;
+			halfIndex += 1;
+			linescorearray[1].push(0);
+			currentouts = 0;
+		  }
+		  if (parseInt(linescoreRaw[i][12]) > 0) {
+			halfRuns += parseInt(linescoreRaw[i][12]);
+			linescorearray[halfIndex%2][parseInt(halfIndex/2)+1] = halfRuns;
+			totals[halfIndex%2][0] += parseInt(linescoreRaw[i][12]);
+		  }
+		  if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('singles') > -1) {
+			totals[halfIndex%2][1] += 1;
+		  }
+		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('doubles') > -1) {
+			totals[halfIndex%2][1] += 1;
+		  }
+		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('triples') > -1) {
+			totals[halfIndex%2][1] += 1;
+		  }
+		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('homers') > -1) {
+			totals[halfIndex%2][1] += 1;
+			if (halfIndex%2 == 0){_this.shadowRoot.querySelector('#away-extra').innerHTML += 'HR: '+linescoreRaw[i][14]+' ('+(linescoreRaw[i][12]-1)+' on)<br>';}
+			else if (halfIndex%2 == 1){_this.shadowRoot.querySelector('#home-extra').innerHTML += 'HR: '+linescoreRaw[i][14]+' ('+(linescoreRaw[i][12]-1)+' on)<br>';}
+		  }
+		  else if (linescoreRaw[i][13] && linescoreRaw[i][13].indexOf('error') > -1) {
+			totals[1 - (halfIndex%2)][2] += 1;
+		  }
+
+		  if (parseInt(linescoreRaw[i][11]) > 0) {
+			currentouts += parseInt(linescoreRaw[i][11]);
+		  }
+		  currentTeam = linescoreRaw[i][15];
+	  }
+	  if (linescorearray[1].length < linescorearray[0].length) {linescorearray[1].push('x');}
+	  linescorearray[0].push(totals[0][0]);
+	  linescorearray[1].push(totals[1][0]);
+	  linescorearray[0].push(totals[0][1]);
+	  linescorearray[1].push(totals[1][1]);
+	  linescorearray[0].push(totals[0][2]);
+	  linescorearray[1].push(totals[1][2]);
+
+
+	var th = document.createElement('th');
+	th.textContent = '';
+	thead.appendChild(th);
+	var td = document.createElement('td');
+	td.textContent = linescorearray[0][0];
+	row1.appendChild(td);
+	td = document.createElement('td');
+	td.textContent = linescorearray[1][0];
+	row2.appendChild(td);
+	for (var i=1;i<linescorearray[0].length-3;i++) {
+		th = document.createElement('th');
+		th.textContent = i;
+		thead.appendChild(th);
+		td = document.createElement('td');
+		td.textContent = linescorearray[0][i];
+		td.id = 'pbpHalf_A-'+i;
+		td.addEventListener('click', e => {alert(e.target.id);});
+		row1.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = linescorearray[1][i];
+		td.id = 'pbpHalf_H-'+i;
+		td.addEventListener('click', e => {alert(e.target.id);});
+		row2.appendChild(td);
+	}
+	['R','H','E'].forEach( x => {
+		th = document.createElement('th');
+		th.textContent = x;
+		thead.appendChild(th);
+		td = document.createElement('td');
+		td.textContent = linescorearray[0][i];
+		row1.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = linescorearray[1][i];
+		row2.appendChild(td);
+		i++;
+	});
+  }
+  
   offbox(batterarray) {
   	var offboxa = this.shadowRoot.querySelector('#awayoff-location');
   	var theada = offboxa.querySelector('thead').querySelector('tr');
@@ -403,8 +476,12 @@ class TabDNBox extends TabDN {
 			this.boxscores.push(retmess[ii][2]);
 		}
 		boxes[0].setAttribute("src",this.boxscores[0]);
+		boxes[0].setAttribute("nsrc",this.boxscores[1]);
+		boxes[0].setAttribute("psrc",this.boxscores[2]);
 		boxes[0].chgsrc();
 		boxes[1].setAttribute("src",this.boxscores[1]);
+		boxes[1].setAttribute("nsrc",this.boxscores[2]);
+		boxes[1].setAttribute("psrc",this.boxscores[0]);
 		boxes[1].chgsrc();
   	}
   	
