0d2cba82c786d1bf27928255b130c47091d12724
triplelog <robmwilc@gmail.com>
2019-10-01 21:28:20 -0400

    f

--- js/tabdnmain.js
+++ js/tabdnmain.js
@@ -67,12 +67,20 @@ class TabDN extends HTMLElement {
 				_this.usecache = false;
 				var jsonmessage = {'command':'load'};
 				_this.ws.send(JSON.stringify(jsonmessage));
-				var jsonmessage = { command: 'filter', formula: 'c28_4/1/2000_c28_9/1/2000@##>##<&' };
-				_this.ws.send(JSON.stringify(jsonmessage));
-				_this.endRow = 162;
-				console.log(_this);
-				var jsonmessage = {'command':'print','startrow':_this.startRow,'endrow':_this.endRow};
-				_this.ws.send(JSON.stringify(jsonmessage));
+				if (_this.tagName == "tabdn-box"){
+					var jsonmessage = { command: 'filter', formula: 'c28_4/1/2000_c28_9/1/2000@##>##<&' };
+					_this.ws.send(JSON.stringify(jsonmessage));
+					_this.endRow = 162;
+					var jsonmessage = {'command':'print','startrow':_this.startRow,'endrow':_this.endRow};
+					_this.ws.send(JSON.stringify(jsonmessage));
+				}
+				else if (_this.tagName == "tabdn-team"){
+					var jsonmessage = { command: 'filter', formula: 'c4_ATL@##=' };
+					_this.ws.send(JSON.stringify(jsonmessage));
+					var jsonmessage = {'command':'print','startrow':_this.startRow,'endrow':_this.endRow};
+					_this.ws.send(JSON.stringify(jsonmessage));
+				}
+				
 				
 			}
 		}
